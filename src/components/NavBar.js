cebb608a9e84cd69ed0ea4ccbd1f2887e98644e5
Omar Khalil <omareldagestany@outlook.com>
2022-06-07 06:27:50 -0400

    setup header and navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+
+
+import React from "react";
+
+const NavBar = () => {
+    return (
+            <nav>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        Tests
+                    </li>
+                </ul>
+            </nav>
+       
+    )
+}
+
+export default NavBar;
+
+
+
+
+
+
+
+
+
+
